6ccc4653febae04f5bb8cd66e1963a320b4509a4
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-12 19:03:35 +0100

    Is my answer correct
    check if user answer correct or not or empty

--- comparition_on_string.sh
+++ comparition_on_string.sh
@@ -2,44 +2,35 @@
 
 # take answer from user
 
+echo "--- Answer in lower case ---"
 echo  "What is the most known DVCS ? "
 
 read -p "answer : " answer
 
-# Check if String is null
 
-# still having a bug here when null typed otherwise work good !
+# below error solved 
+# when user type nothing cuz a bug -*-
 
-if (($answer==-z))
+
+# check if input is null
+
+if [ -z $answer ]
 
 then
-    echo "not null"
-
+    echo "you submit an empty answer !"
 else
-    echo "null"
-    answer=null
-
-fi
-
+    
 # check correction
 
-if [ $answer == "git" ]
+    if [[ $answer == "git" ]]
 
-then
-    echo "You are Right , ${answer} is a distributed version control system ."
-else
-    echo "You wrong , ${answer} is not the most famouse distributed version control system ."
-fi
+    then
+	echo "You are Right , ${answer} is the most known distributed version control system ."
+    else
+	echo "You wrong , ${answer} is not the most famouse distributed version control system ."
+    fi
 
-# check order ASCII
-
-if [[ "r" > "n" ]]
-
-then
-    echo "r is after n"
-else
-    echo "r is before n"
-
+    
 fi
 
 
